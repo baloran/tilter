2b88fc0634c9773b6cab49b87dd7163e1a88b9d4
Tom Bonnike <bonniketom@gmail.com>
2016-12-07 19:44:12 +0100

    chore(linting): Fixed linting settings

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
-RSpec.describe "User", type: :model do
-  it "create an user" do
-    baloran = User.create(username: "baloran", password: "password", email: "baloranandco@gmail.com")
+RSpec.describe 'User', type: :model do
+  it 'create an user' do
+    baloran = User.create(
+      username: 'baloran',
+      password: 'password',
+      email: 'baloranandco@gmail.com'
+    )
 
-    expect(baloran.email).to eq("baloranandco@gmail.com")
+    expect(baloran.email).to eq('baloranandco@gmail.com')
   end
 end

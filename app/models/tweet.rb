34eae762edbdecefd707ce99ff96d939e626468e
Tom Bonnike <bonniketom@gmail.com>
2016-12-07 22:08:42 +0100

    feat(tweet): Added content validation
    And added tests

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,17 @@
+# Unfortunately, Tilt is a reserved name in RoR. We will respect Twitter’s
+# naming instead and only the front-end and routes will show Tilt/Retilt, etc…
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  MIN_CONTENT_LENGTH = 1
+  MAX_CONTENT_LENGTH = 139 # Not 140 cuz u gotta tilt m8
+  validates :content, length: {
+    minimum: MIN_CONTENT_LENGTH,
+    maximum: MAX_CONTENT_LENGTH,
+    message: %(
+      Your tilt must be between #{MIN_CONTENT_LENGTH} and
+      #{MAX_CONTENT_LENGTH} characters
+    ),
+    allow_nil: false, allow_blank: false
+  }
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
+  it 'can be created by an user' do
+    user = create_valid_user
+
+    Tweet.create(content: 'I’m really tilting today!', user_id: user.id)
+
+    found = Tweet.joins(:user).last
+    expect(found.content).to eq('I’m really tilting today!')
+    expect(found.user).to eq(user)
+  end
+
+  it 'has a content between 1 and 139 characters' do
+    user = create_valid_user
+
+    tweet = Tweet.create(user_id: user.id)
+    expect(tweet.valid?).to eq(false)
+
+    tweet.content = 'I'
+    expect(tweet.valid?).to eq(true)
+
+    tweet.content = 'I’m really tilting today!'
+    expect(tweet.valid?).to eq(true)
+
+    tweet.content = (0..500).map { 'X' }.join
+    expect(tweet.valid?).to eq(false)
+  end
+end
+
+# Returns a valid user to avoid repetitions in tests
+def create_valid_user
+  User.create(
+    username: 'baloran',
+    display_name: 'bal0ran',
+    password: 'password',
+    email: 'baloranandco@gmail.com'
+  )
 end

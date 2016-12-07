f1601b1dc7924c1192ec0f9ec34e0af5060533de
Tom Bonnike <bonniketom@gmail.com>
2016-12-07 20:49:12 +0100

    feat(user): Added tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :trackable, :validatable
+
+  MAX_DESCRIPTION_LENGTH = 200
+  validates :description, length: {
+    maximum: MAX_DESCRIPTION_LENGTH,
+    message:
+      "Your description must be less than #{MAX_DESCRIPTION_LENGTH} characters",
+    allow_nil: false, allow_blank: true
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207182032) do
+ActiveRecord::Schema.define(version: 20161207193257) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,11 +33,12 @@ ActiveRecord::Schema.define(version: 20161207182032) do
     t.datetime "updated_at",                          null: false
     t.string   "username",               default: "", null: false
     t.string   "display_name",           default: "", null: false
-    t.string   "description"
+    t.string   "description",            default: ""
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end

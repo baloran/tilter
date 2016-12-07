2732cf890e8d63a3807c49d62b9da598bdf68517
Tom Bonnike <bonniketom@gmail.com>
2016-12-07 21:36:00 +0100

    feat(user): Added validations for username & display_name
    And added tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,32 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable
 
+  before_validation :assign_display_name
+
+  MIN_USERNAME_LENGTH = 2
+  MAX_USERNAME_LENGTH = 40
+  validates :username, length: {
+    minimum: MIN_USERNAME_LENGTH,
+    maximum: MAX_USERNAME_LENGTH,
+    message: %(
+      Your username must be between #{MIN_USERNAME_LENGTH} and
+      #{MAX_USERNAME_LENGTH} characters
+    ),
+    allow_nil: false, allow_blank: false
+  }
+
+  MIN_DISPLAY_NAME_LENGTH = 2
+  MAX_DISPLAY_NAME_LENGTH = 40
+  validates :display_name, length: {
+    minimum: MIN_DISPLAY_NAME_LENGTH,
+    maximum: MAX_DISPLAY_NAME_LENGTH,
+    message: %(
+      Your display name must be between #{MIN_DISPLAY_NAME_LENGTH} and
+      #{MAX_DISPLAY_NAME_LENGTH} characters
+    ),
+    allow_nil: false, allow_blank: false
+  }
+
   MAX_DESCRIPTION_LENGTH = 200
   validates :description, length: {
     maximum: MAX_DESCRIPTION_LENGTH,
@@ -11,4 +37,8 @@ class User < ApplicationRecord
       "Your description must be less than #{MAX_DESCRIPTION_LENGTH} characters",
     allow_nil: false, allow_blank: true
   }
+
+  def assign_display_name
+    self.display_name = username if display_name.blank?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe 'User', type: :model do
   it 'can be created' do
     User.create(
       username: 'baloran',
+      display_name: 'bal0ran',
       password: 'password',
       email: 'baloranandco@gmail.com'
     )
 
     found = User.last
     expect(found.username).to eq('baloran')
+    expect(found.display_name).to eq('bal0ran')
     expect(found.email).to eq('baloranandco@gmail.com')
   end
 
@@ -28,6 +30,17 @@ RSpec.describe 'User', type: :model do
 
     user.password = 'password'
     expect(user.valid?).to eq(true)
+  end
+
+  it 'is automatically assigned its username as display name if none is set' do
+    user = User.create(
+      username: 'baloran',
+      password: 'password',
+      email: 'baloranandco@gmail.com'
+    )
+
+    expect(user.username).to eq('baloran')
+    expect(user.display_name).to eq('baloran')
   end
 
   it 'can’t have the same email as another user' do
@@ -52,7 +65,6 @@ RSpec.describe 'User', type: :model do
   it 'can’t have the same username as another user' do
     user = User.create(
       username: 'baloran',
-      display_name: 'bal0ran',
       password: 'password',
       email: 'baloranandco@gmail.com'
     )
@@ -63,17 +75,55 @@ RSpec.describe 'User', type: :model do
     expect do
       User.create(
         username: 'baloran',
-        display_name: 'bal0ran',
         password: 'password',
         email: 'stammeister@gmail.com'
       )
     end.to raise_exception(ActiveRecord::RecordNotUnique)
   end
 
+  it 'can have a username between 2 and 40 characters' do
+    user = User.create(
+      display_name: 'baloran',
+      password: 'password',
+      email: 'baloranandco@gmail.com'
+    )
+    expect(user.valid?).to eq(false)
+
+    user.username = 'b'
+    expect(user.valid?).to eq(false)
+
+    user.username = (0..100).map { 'X' }.join
+    expect(user.valid?).to eq(false)
+
+    user.username = 'ba'
+    expect(user.valid?).to eq(true)
+
+    user.username = 'baloran'
+    expect(user.valid?).to eq(true)
+  end
+
+  it 'can have a display name between 2 and 40 characters' do
+    user = User.create(
+      username: 'baloran',
+      display_name: 'b',
+      password: 'password',
+      email: 'baloranandco@gmail.com'
+    )
+    expect(user.valid?).to eq(false)
+
+    user.display_name = (0..100).map { 'X' }.join
+    expect(user.valid?).to eq(false)
+
+    user.display_name = 'ba'
+    expect(user.valid?).to eq(true)
+
+    user.display_name = 'baloran'
+    expect(user.valid?).to eq(true)
+  end
+
   it 'requires a valid email' do
     user = User.new
     user.username = 'baloran'
-    user.display_name = 'bal0ran'
     user.password = 'password'
 
     user.email = 'baloranandco'
@@ -89,7 +139,6 @@ RSpec.describe 'User', type: :model do
   it 'can have a description that has a maximum of 200 characters' do
     user = User.create(
       username: 'baloran',
-      display_name: 'bal0ran',
       password: 'password',
       email: 'baloranandco@gmail.com'
     )

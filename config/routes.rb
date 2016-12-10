020c966ceac4f230f973101e0f9cb632c3aab116
baloran <contact@baloran.fr>
2016-12-10 22:33:20 +0100

    [WIP] chore(gem): add better error + rpl live console + meta_request
    - meta_request for rails console

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
+    registrations: 'users/registrations'
   }
 
   get '/:username', to: 'users/profiles#profile'

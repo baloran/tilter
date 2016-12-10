2cda546fcce45758ea3aec1e0db18b0ce064e056
Tom Bonnike <bonniketom@gmail.com>
2016-12-10 20:31:25 +0100

    feat(routes): Added all routes

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,9 @@
+class TweetsController < ApplicationController
+  def index
+    # Get all tilts here
+  end
+
+  def show
+    # Get a single tilt here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
+  get '/' => 'tweets#index'
+  get '/tilt/:id' => 'tweets#show'
+
+  get '/user/settings' => 'users#settings'
+  get '/user/:id' => 'users#show'
+  get '/user/:id/following' => 'users#following'
+  get '/user/:id/followers' => 'users#followers'
+  get '/user/:id/likes' => 'users#likes'
 end

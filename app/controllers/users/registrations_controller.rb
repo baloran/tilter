6970fd3e1bdc28d5cd43cda23bb0a8b78b8b7414
baloran <contact@baloran.fr>
2016-12-11 23:05:01 +0100

    feat(udpate): add username to update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -50,9 +50,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+                                        :email,
+                                        :username,
+                                        :display_name
+                                      ])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)

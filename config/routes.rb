6bff3ee04799dca6a1a4cebb685e7b0392a0ef02
Tom Bonnike <bonniketom@gmail.com>
2016-12-11 17:31:32 +0100

    feat(header): Made header dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'tweets#index'
+  get '/' => 'tweets#index', as: :root
   get 'tilts' => redirect('/')
   resources :tilts, :controller => 'tweets'
 

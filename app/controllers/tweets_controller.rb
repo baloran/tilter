1f1d44d5a96ab0316814d2766988c021f2c73617
Tom Bonnike <bonniketom@gmail.com>
2016-12-18 21:04:38 +0100

    feat(tweets): Fixed list of displayed tweets not including only followed users

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,10 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tilts = Tweet.roots
+    following_list = current_user.follow_ids
+    following_list.push(current_user.id)
+
+    @tilts = Tweet.roots.where(user_id: following_list)
   end
 
   def show
